90c39155c4cc22034416f9fd3efb3f73917bbb6b
Ivan Laushkin <laushkinivan1@gmail.com>
2025-02-07 11:10:07 +0100

    modified player.py

--- player.py
+++ player.py
@@ -2,7 +2,7 @@ import pygame
 
 from load import LoadFile
 from sound import Sounds
-from settings import SCREEN_HEIGHT
+from settings import SCALE, SCREEN_HEIGHT
 
 class Player(pygame.sprite.Sprite):
     player_y = SCREEN_HEIGHT/2
@@ -18,9 +18,9 @@ class Player(pygame.sprite.Sprite):
         self.bird_indx = 0
         self.angle = 0
 
-        self.gravity = -15
+        self.gravity = -8*SCALE
         self.image = self.birds[self.bird_indx]
-        self.rect = self.image.get_rect(center = (100, SCREEN_HEIGHT/2))
+        self.rect = self.image.get_rect(center = (50*SCALE, SCREEN_HEIGHT/2))
 
     def animation(self):
         self.bird_indx += 0.2
@@ -30,12 +30,15 @@ class Player(pygame.sprite.Sprite):
 
     def player_jump(self) -> None:
         self.jump_sound.play()
-        self.gravity = -15
+        self.gravity = -8*SCALE
 
     def apply_gravity(self) -> None:
-        self.gravity += 1
+        self.gravity += 0.5*SCALE
         if self.gravity <= 36:
-            self.player_rotate(self.gravity*2.5)
+            if SCALE == 1:
+                self.player_rotate(self.gravity*4.5)
+            else:
+                self.player_rotate(self.gravity*2.5)
         else: 
             self.player_rotate(90)
         self.rect.y += self.gravity
@@ -46,9 +49,9 @@ class Player(pygame.sprite.Sprite):
         Player.player_angle = -angle
     
     def new_game(self):
-        self.gravity = -15
+        self.gravity = -8*SCALE
         self.image = self.birds[0]
-        self.rect = self.image.get_rect(center = (100, SCREEN_HEIGHT/2))
+        self.rect = self.image.get_rect(center = (50*SCALE, SCREEN_HEIGHT/2))
 
     def update(self):
         self.animation()
