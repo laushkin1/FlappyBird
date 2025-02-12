433756b627d89f0eee98ec25a4940361126cfb8c
Ivan Laushkin <laushkinivan1@gmail.com>
2025-02-12 12:38:21 +0100

    add sound.py

--- game/sound.py
+++ game/sound.py
@@ -5,24 +5,24 @@ from game.settings import VOLUME
 
 assets_path = os.path.join(os.path.dirname(__file__), "flappy-bird-assets", "audio")
 
-class Sounds():
+
+class Sounds:
     def jump(self):
-        s = pygame.mixer.Sound(assets_path + '/wing.wav')
-        s.set_volume(VOLUME/100)
+        s = pygame.mixer.Sound(assets_path + "/wing.wav")
+        s.set_volume(VOLUME / 100)
         return s
 
     def point(self):
-        s = pygame.mixer.Sound(assets_path + '/point.wav')
-        s.set_volume(VOLUME/100)
+        s = pygame.mixer.Sound(assets_path + "/point.wav")
+        s.set_volume(VOLUME / 100)
         return s
 
     def hit(self):
-        s = pygame.mixer.Sound(assets_path + '/hit.wav')
-        s.set_volume(VOLUME/100)
+        s = pygame.mixer.Sound(assets_path + "/hit.wav")
+        s.set_volume(VOLUME / 100)
         return s
 
     def die(self):
-        s = pygame.mixer.Sound(assets_path + '/die.wav')
-        s.set_volume(VOLUME/100)
+        s = pygame.mixer.Sound(assets_path + "/die.wav")
+        s.set_volume(VOLUME / 100)
         return s
-
