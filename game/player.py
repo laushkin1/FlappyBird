115b1e9afa5f78d8c3c252820899f30e47d843dd
Ivan Laushkin <laushkinivan1@gmail.com>
2025-02-12 12:37:03 +0100

    add player.py

--- game/player.py
+++ game/player.py
@@ -4,9 +4,11 @@ from game.load import LoadFile
 from game.sound import Sounds
 from game.settings import SCALE, SCREEN_HEIGHT
 
+
 class Player(pygame.sprite.Sprite):
-    player_y = SCREEN_HEIGHT/2
+    player_y = SCREEN_HEIGHT / 2
     player_angle = 0
+
     def __init__(self, color) -> None:
         super().__init__()
 
@@ -18,9 +20,9 @@ class Player(pygame.sprite.Sprite):
         self.bird_indx = 0
         self.angle = 0
 
-        self.gravity = -8*SCALE
+        self.gravity = -8 * SCALE
         self.image = self.birds[self.bird_indx]
-        self.rect = self.image.get_rect(center = (50*SCALE, SCREEN_HEIGHT/2))
+        self.rect = self.image.get_rect(center=(50 * SCALE, SCREEN_HEIGHT / 2))
 
     def animation(self):
         self.bird_indx += 0.2
@@ -30,16 +32,16 @@ class Player(pygame.sprite.Sprite):
 
     def player_jump(self) -> None:
         self.jump_sound.play()
-        self.gravity = -8*SCALE
+        self.gravity = -8 * SCALE
 
     def apply_gravity(self) -> None:
-        self.gravity += 0.5*SCALE
+        self.gravity += 0.5 * SCALE
         if self.gravity <= 36:
             if SCALE == 1:
-                self.player_rotate(self.gravity*4.5)
+                self.player_rotate(self.gravity * 4.5)
             else:
-                self.player_rotate(self.gravity*2.5)
-        else: 
+                self.player_rotate(self.gravity * 2.5)
+        else:
             self.player_rotate(90)
         self.rect.y += self.gravity
 
@@ -47,13 +49,12 @@ class Player(pygame.sprite.Sprite):
         self.angle = -angle
         self.image = pygame.transform.rotate(self.image, -angle)
         Player.player_angle = -angle
-    
+
     def new_game(self):
-        self.gravity = -8*SCALE
+        self.gravity = -8 * SCALE
         self.image = self.birds[0]
-        self.rect = self.image.get_rect(center = (50*SCALE, SCREEN_HEIGHT/2))
+        self.rect = self.image.get_rect(center=(50 * SCALE, SCREEN_HEIGHT / 2))
 
     def update(self):
         self.animation()
         self.apply_gravity()
-
